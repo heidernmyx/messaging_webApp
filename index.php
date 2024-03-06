e9a22fb70d22dee989f8bbb05c7c189d9918f797
heidernmyx <hmontejo123@gmail.com>
2024-03-07 00:05:34 +0800

    login backend

--- index.php
+++ index.php
@@ -21,16 +21,16 @@
       <div>
         <img id="logo" src="./assets/images/userImage.png" alt="">
       </div>
-      <form action="">
+      <form action="php/login.php" method="post">
         <div id="">
           <label class="textfield_label" for="username">Username: </label><br><div class="spacer"></div>
-          <input type="text" name="username" id="username" placeholder="Enter Username">
+          <input type="text" name="username" id="username" placeholder="Enter Username" required>
         <br> <br>
           <label class="textfield_label" for="password">Password: </label><br><div class="spacer"></div>
-            <input type="password" name="password" id="password" placeholder="Enter Password">
+            <input type="password" name="password" id="password" placeholder="Enter Password" required>
         </div>
         <div style="height: 20px;"></div>
-        <button id="login_button" name="login_button">Log In</button>
+        <button id="login_button" name="login_button" >Log In</button>
         <div id="newSpacer" style="height: 30px"></div>
         <label class="register" id="register" style="color: gainsboro; font-size: 15;">Don't have an account yet? 
         <a class="register" id="register_href" href="./html/register.html">Register here</a>
--- php/login.php
+++ php/login.php
@@ -1,3 +1,30 @@
 <?php
 
 include '../include/connection.php' ;
+
+// ? Backend processes for login
+
+$username_input = $_POST['username'];
+$password_input = $_POST['password'];
+
+
+// check username and password
+$sql = "SELECT * FROM `tbl_accounts` WHERE username = :username AND password = :password";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':username', $username_input);
+$stmt->bindParam(':password', $password_input);
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($result) {
+  if($username_input == $result['username'] && $password_input == $result['password']) {
+    echo "login successfully";
+  }
+} else {
+    echo "Invalid username or password";
+}
+
+
+
+
+
