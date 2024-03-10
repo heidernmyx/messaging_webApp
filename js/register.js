be3ea7fe3e4a9400a4fe99dad651addbfbc1bdb1
heidernmyx <hmontejo123@gmail.com>
2024-03-10 21:14:46 +0800

    few update

--- js/register.js
+++ js/register.js
@@ -1,36 +1,10 @@
-// const span = document.getElementsByClassName('.close');
-
-// if(span){
-//   console.log('true1');
-//   if (modal) {
-//     console.log('true2');
-//   }
-// }
-// span.addEventListener('click', () =>{
-//   modal.style.display = none;
-//   alert('clicked');
-// })
-// const span = document.getElementById('#close');
-let span = document.getElementById('#close');
-console.log(span);
-if (span) {
-  console.log(true);
-}
-else{
-  console.log("error");
-}
-
-// span.addEventListener('click', function () {
-//   close();
-// })
-
-// const close = () => {
-//   const modal = document.querySelector('modal').style.display = "none";
-// }
 
 
-let modal = document.querySelector('.modal');
-span.addEventListener("click", () =>{
-  modal.style.display = 'none'; // Make sure 'none' is enclosed in quotes
-  alert('clicked');
-});
+  const spanClass = document.querySelector('.close_button');
+  const modal = document.querySelector('.modal');
+  spanClass.addEventListener("click", () => {
+    modal.style.display = 'none';
+  });
+
+
+  const 
