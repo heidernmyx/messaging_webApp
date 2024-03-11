f21d7dcb005db3eae0993ff916aa362ede1b0294
heidernmyx <hmontejo123@gmail.com>
2024-03-11 09:30:02 +0800

    update (wont return to index.html (login page))

--- js/register.js
+++ js/register.js
@@ -4,8 +4,23 @@ spanClass.addEventListener("click", () => {
   modal.style.display = 'none';
 });
 
+const show_modal = () => {
+  document.querySelector('.modal').style.display = "block";
+    document.querySelector('.modal_content').style.display = "block";
+    setTimeout(function() {
+      console.log("test");
+      window.open('index.html');
+    }, 5000);
+    alert('test');
+}
 
-const register_button = document.getElementById('register_button');
-  register_button.addEventListener('click', () => {
-  document.querySelector('.modal').style.display = "";
+
+document.getElementById('regForm').addEventListener('submit', (event) =>{
+  event.preventDefault();
+  console.log('run');
+  const register_button = document.getElementById('register_button');
+  register_button.addEventListener('click', show_modal());
+  register_button();
+  event.;
 })
+
